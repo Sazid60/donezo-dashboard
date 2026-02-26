c7fc45c78e72c94bc05c1c29097d1a06df4023ee
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2026-02-26 07:24:51 +0600

    Project Initiated With Folder Structure and dependecies Intallation and Necessary Files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Outlet, ScrollRestoration } from 'react-router';
+
+function App() {
+  return (
+    <>
+      <ScrollRestoration />
+      <Outlet />
+    </>
+  )
+}
+
+export default App
